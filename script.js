8d4494bc8e6be1487ffa8e5742be66fcba45bb31
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-21 09:29:36 -0300

    Requisito 7 - Clicar em uma das cores da paleta, faz com somente ela seja selecionada

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Project Pixel Arts');
+const bgBlack = document.querySelector('.bg-black');
+bgBlack.classList.add('selected');
+
+const colorPalette = document.getElementsByClassName('color');
+
+function addClassSelected(item) {
+  item.classList.add('selected');
+}
+
+function removeClassSelected(collection) {
+  for (let index = 0; index < collection.length; index += 1) {
+    collection[index].classList.remove('selected');
+  }
+}
+
+for (let index = 0; index < colorPalette.length; index += 1) {
+  colorPalette[index].addEventListener('click', (event) => {
+    removeClassSelected(colorPalette);
+    addClassSelected(event.target);
+  });
+}
+
+// console.log(colorPalette);
